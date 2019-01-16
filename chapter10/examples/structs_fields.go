6905125c842f6e171ace26571e00184e08888c42
EvanXzj <2686465170@qq.com>
2019-01-16 15:28:32 +0800

    have a break

---
+++ chapter10/examples/structs_fields.go
@@ -0,0 +1,23 @@
+// Go 语言中，结构体和它所包含的数据在内存中是以连续块的形式存在的，即使结构体中嵌套有其他的结构体，这在性能上带来了很大的优势。
+// 不像 Java 中的引用类型，一个对象和它里面包含的对象可能会在不同的内存空间中，这点和Go语言中的指针很像
+package main
+
+import "fmt"
+
+type struct1 struct {
+	i1 int
+	f1 float32
+	str string
+}
+
+func main() {
+	ms := new(struct1)
+	ms.i1 = 10
+	ms.f1 = 15.5
+	ms.str = "Chris"
+
+	fmt.Printf("The int is: %d\n", ms.i1)
+    fmt.Printf("The float is: %f\n", ms.f1)
+    fmt.Printf("The string is: %s\n", ms.str)
+    fmt.Println(ms)
+}
