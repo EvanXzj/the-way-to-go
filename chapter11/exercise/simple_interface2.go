bcf380d6fa56d9c4a9ac8bd194023f1260210213
EvanXzj <2686465170@qq.com>
2019-01-21 16:21:19 +0800

    ch11.4 done

---
+++ chapter11/exercise/simple_interface2.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type Simpler interface {
+	Get() int
+	Set(int)
+}
+
+type Simple struct {
+	i int
+}
+
+func (s *Simple) Get() int {
+	return s.i
+}
+
+func (s *Simple) Set(u int) {
+	s.i = u
+}
+
+type RSimple struct {
+	i, j int
+}
+
+func (r *RSimple) Get() int {
+	return r.j
+}
+
+func (r *RSimple) Set(u int) {
+	r.j = u
+}
+
+func fI(it Simpler) int {
+	switch it.(type) {
+	case *Simple:
+		it.Set(5)
+		return it.Get()
+	case *RSimple:
+		it.Set(50)
+		return it.Get()
+	default:
+		return 99
+	}
+}
+
+func main() {
+	var s Simple
+	fmt.Println(fI(&s)) // &s is required because Get() is defined with a receiver type pointer
+	var r RSimple
+	fmt.Println(fI(&r))
+}
