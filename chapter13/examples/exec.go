230d9c6f52475810c80911946cdf60e628aa5c02
EvanXzj <2686465170@qq.com>
2019-01-28 14:39:49 +0800

    ch13.06 done

---
+++ chapter13/examples/exec.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// 1) os.StartProcess //
+	/*********************/
+	/* Linux: */
+	env := os.Environ()
+	// fmt.Println(env)
+	procAttr := &os.ProcAttr{
+		Env: env,
+		Files: []*os.File{
+			os.Stdin,
+			os.Stdout,
+			os.Stderr,
+		},
+	}
+	// 1st example: list files
+	pid, err := os.StartProcess("/bin/ls", []string{"ls", "-l"}, procAttr)
+	if err != nil {
+		fmt.Printf("Error %v starting process!", err) //
+		os.Exit(1)
+	}
+	fmt.Printf("The process id is %v", pid)
+}
